d3e85078692fe34ce984db6cf77a94ca60995947
Trần Nguyễn Tuấn Anh <43093213+dopaemon@users.noreply.github.com>
2021-08-01 13:17:46 +0700

    Create qilizz.php

---
+++ qilizz.php
@@ -0,0 +1,144 @@
+date_default_timezone_set("Asia/Ho_Chi_Minh"); 
+error_reporting(0);
+ session_start();
+$green="\033[1;32m";
+$wait = $green."Đợi";
+$res="\033[0m";
+$red="\033[1;31m";
+$yellow="\033[0;93m";
+$lightblue="\033[1;35m";
+$bluelight="\033[1;34m";
+$blue="\033[1;36m";
+$purple="\e[35m";
+$end="\033[0m";
+$do="\033[1;91m";
+$luc="\033[1;92m";
+$vang="\033[1;93m";
+$xduong="\033[1;94m";
+$hong="\033[1;95m";
+$trang="\033[1;97m";
+$BBlack="\033[1;30m" ;      
+$BRed="\033[1;31m"         ;
+$BGreen="\033[1;32m"       ;
+$BYellow="\033[1;33m"   ;
+$BBlue="\033[1;34m"        ;
+$BPurple="\033[1;35m"      ;
+$BCyan="\033[1;36m"   ;
+$BWhite="\033[1;37m"    ;
+$Blue="\033[0;34m";
+$res="\033[0m";
+$end="\033[0m";
+$red="\033[1;31m";
+$xanh="\033[1;32m";
+$cyan="\033[1;36m";
+$yellow="\033[1;33m";
+$turquoise="\033[1;34m";
+$maugi="\033[1;35m";
+$white= "\033[1;37m";
+$red="\033[0;31m";
+$redb="\033[1;31m";
+$green="\033[0;32m";
+$greenb="\033[1;32m";
+$yellow="\033[0;33m";
+$yellowb="\033[1;33m";
+$syan="\033[1;36m";
+$blue="\033[0;34m";
+$blueb="\033[1;34m";
+$purple="\033[0;35m";
+$purpleb="\033[1;35m";
+$lightblue="\033[0;36m";
+$lightblue="\033[1;35m";
+$lightblueb="\033[1;36m";
+$input = array($d2="\033[1;36m",$tmd3="\033[1;37m",$tmd4="\033[1;37m",$tmd5="\033[0;31m",$tmd6="\033[1;31m",$tmd7="\033[0;32m",$tmd8="\033[1;32m",$tmd9="\033[0;33m",$tmd10="\033[1;33m",$tmd11="\033[0;34m",$tmd12="\033[1;34m",$tmd13="\033[0;35m",$tmd14="\033[1;35m",$tmd15="\033[0;36m",$tmd16="\033[1;36m");
+$rand_keys = array_rand($input, 10);
+$input = array($tmd1="\033[1;46m",$tmd2="\033[1;36m",$tmd3="\033[1;37m",$tmd4="\033[1;37m",$tmd5="\033[0;31m",$tmd6="\033[1;31m",$tmd7="\033[0;32m",$tmd8="\033[1;32m",$tmd9="\033[0;33m",$tmd10="\033[1;33m",$tmd11="\033[0;34m",$tmd12="\033[1;34m",$tmd13="\033[0;35m",$tmd14="\033[1;35m",$tmd15="\033[0;36m",$tmd16="\033[1;36m");
+$rand_keys = array_rand($input, 15);
+$end="\033[0m";
+$black="\033[0;30m";
+$blackb="\033[1;30m";
+$white="\033[0;37m";
+$whiteb="\033[1;37m";
+$red="\033[0;31m";
+$redb="\033[1;31m";
+$green="\033[0;32m";
+$greenb="\033[1;32m";
+$yellow="\033[0;33m";
+$yellowb="\033[1;33m";
+$syan="\033[1;36m";
+$blue="\033[0;34m";
+$blueb="\033[1;34m";
+$purple="\033[0;35m";
+$purpleb="\033[1;35m";
+$lightblue="\033[0;36m";
+$lightblue="\033[1;35m";
+$lightblueb="\033[1;36m";
+$input = array($d2="\033[1;36m",$tmd3="\033[1;37m",$tmd4="\033[1;37m",$tmd5="\033[0;31m",$tmd6="\033[1;31m",$tmd7="\033[0;32m",$tmd8="\033[1;32m",$tmd9="\033[0;33m",$tmd10="\033[1;33m",$tmd11="\033[0;34m",$tmd12="\033[1;34m",$tmd13="\033[0;35m",$tmd14="\033[1;35m",$tmd15="\033[0;36m",$tmd16="\033[1;36m");
+$rand_keys = array_rand($input, 10);
+$input = array($tmd1="\033[1;46m",$tmd2="\033[1;36m",$tmd3="\033[1;37m",$tmd4="\033[1;37m",$tmd5="\033[0;31m",$tmd6="\033[1;31m",$tmd7="\033[0;32m",$tmd8="\033[1;32m",$tmd9="\033[0;33m",$tmd10="\033[1;33m",$tmd11="\033[0;34m",$tmd12="\033[1;34m",$tmd13="\033[0;35m",$tmd14="\033[1;35m",$tmd15="\033[0;36m",$tmd16="\033[1;36m");
+$rand_keys = array_rand($input, 15);
+
+$banner="\r\033[1;96m███\033[1;93m╗   \033[1;96m██\033[1;93m╗ \033[1;96m█████\033[1;93m╗ \033[1;96m███\033[1;93m╗   \033[1;96m███\033[1;93m╗    
+\033[1;31m████\033[1;93m╗  \033[1;31m██\033[1;93m║\033[1;31m██\033[1;93m╔══\033[1;31m██\033[1;93m╗\033[1;31m████\033[1;93m╗ \033[1;31m████\033[1;93m║    
+\033[1;95m██\033[1;93m╔\033[1;95m██\033[1;93m╗ \033[1;95m██\033[1;93m║\033[1;95m███████\033[1;93m║\033[1;95m██\033[1;93m╔\033[1;95m████\033[1;93m╔\033[1;95m██\033[1;93m║    
+\033[1;97m██\033[1;93m║╚\033[1;97m██\033[1;93m╗\033[1;97m██\033[1;93m║\033[1;97m██\033[1;93m╔══\033[1;97m██\033[1;93m║\033[1;97m██\033[1;93m║╚\033[1;97m██\033[1;93m╔╝\033[1;97m██\033[1;93m║    
+\033[1;37m██\033[1;93m║ \033[1;93m╚\033[1;37m████\033[1;93m║\033[1;37m██\033[1;93m║  \033[1;37m██\033[1;93m║\033[1;37m██\033[1;93m║ ╚═╝ \033[1;37m██\033[1;93m║    
+\033[1;93m╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝     ╚═╝  
+\033[1;41;33mBản Quyền Thuộc Nam Tool\033[0m\033[1;30m
+\033[1;33m╚═══════════════════════════════════════════════════╝\n\n";
+$logo="\033[1;33m─────────────────────────────────────────────────────────────────
+\033[1;31m[".$luc."●".$do."] ".$trang."=> \033[1;93mTool TraoDoiSub Đơn Token
+\033[1;31m[".$luc."●".$do."] ".$trang."=> \033[1;97mTool Bản Quyền : \033[1;93mNam Tool
+\033[1;31m[".$luc."●".$do."] ".$trang."=> \033[1;97mZalo: \033[1;32m0852048795
+\033[1;31m[".$luc."●".$do."] ".$trang."=> \033[1;97mMoMo: 0328485126
+\033[1;33m─────────────────────────────────────────────────────────────────\n";
+@system('clear');
+echo $banner;
+echo $logo;
+echo "\n\033[1;34m•\033[1;37m ID Facebook Cần Tăng: ";
+$id = trim(fgets(STDIN)); 
+echo "\n\033[1;34m•\033[1;37m Cookie Qlizz: ";
+$cookie = trim(fgets(STDIN)); 
+$idsub = $id;
+$n = '10000000';
+system("clear");
+echo $banner;
+echo $logo;
+echo "\033[1;31m* \033[1;33mĐang Tăng Cho ID ".$id."\033[1;37m\n\n";
+echo "\033[1;33m─────────────────────────────────────────────────────────────────\n";
+$i = 0;
+while ($i++ < $n && $id > 0) {
+	
+	 	 echo $green; echo"\033[1;31m[\033[1;32m".$i."\033[1;31m] \033[0;33m Buff Follow Thành Công Cho ID: \033[1;32m".$id."\n";
+	sleep(1);
+  $ch = curl_init();
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, 'https://qlizz.com/autofollowers');  
+    curl_setopt($ch, CURLOPT_HEADER, 1);
+    $headers = array();
+    $headers[] = 'Content-Type: application/xhtml+xml';
+    $headers[] = 'Cookie: ' . $cookie;
+    $headers[] = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) coc_coc_browser/77.0.126 Chrome/71.0.3578.126 Safari/537.36';
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    $result = curl_exec($ch);
+    $token = explode('"', explode('<input type="hidden" name="_token" value="', $result)[1])[0];
+    $ch1 = curl_init();
+    curl_setopt($ch1, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch1, CURLOPT_URL, 'https://qlizz.com/send');  
+    curl_setopt($ch1, CURLOPT_POSTFIELDS, "_token=$token&submit_id=$idsub&tool=autofollowers");
+    curl_setopt($ch1, CURLOPT_POST, 1);
+    curl_setopt($ch1, CURLOPT_HEADER, 1);
+    $headers = array();
+    $headers[] = 'Content-Type: application/x-www-form-urlencoded';
+    $headers[] = 'Cookie: ' . $cookie;
+    $headers[] = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) coc_coc_browser/77.0.126 Chrome/71.0.3578.126 Safari/537.36';
+    curl_setopt($ch1, CURLOPT_HTTPHEADER, $headers);
+    $result1 = curl_exec($ch1);
+ $input = array($tmd1="\033[1;46m",$tmd2="\033[1;36m",$tmd3="\033[1;37m",$tmd4="\033[1;37m",$tmd5="\033[0;31m",$tmd6="\033[1;31m",$tmd7="\033[0;32m",$tmd8="\033[1;32m",$tmd9="\033[0;33m",$tmd10="\033[1;33m",$tmd11="\033[0;34m",$tmd12="\033[1;34m",$tmd13="\033[0;35m",$tmd14="\033[1;35m",$tmd15="\033[0;36m",$tmd16="\033[1;36m");
+$rand_keys = array_rand($input, 15);
+
+for ($giay=1800;$giay>-1;$giay--){
+echo $green."\033[1;32mVui Lòng Đợi\033[1;33m $giay \033[1;32mGiây";
+sleep(1);
+echo "\r";
+}
+}
